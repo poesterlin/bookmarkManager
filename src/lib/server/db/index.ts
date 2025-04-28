b01a224d107df5981879b6b7bf0defcc5f0b5a87
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-04-28 11:24:25 +0200

    switch to bun sql client

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,15 @@
 import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { SQL } from 'bun';
 
 if (!env.DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
 }
 
-export const db = drizzle(env.DATABASE_URL, { logger: false });
+const client = new SQL(env.DATABASE_URL) as any;
+client.options = {
+	parsers: {},
+	serializers: {},
+};
+
+export const db = drizzle({ client, logger: false });
