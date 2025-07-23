e4616fbb9ca8709de01b12599a5a91754699687b
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-07-24 00:56:16 +0200

    Enhances tag filtering and scroll behavior
    Improves the tag filtering experience by allowing users to toggle tag filters.
    
    Introduces a sticky tag bar that clamps its vertical position based on scroll to enhance usability.
    Also adds touch event listeners to track user interaction and modifies the scrolling speed accordingly.

--- src/lib/client/app.svelte.ts
+++ src/lib/client/app.svelte.ts
@@ -1,9 +1,23 @@
 class AppState {
 	isDarkMode = $state(false);
+	yClamped = $state(0);
+	isTouching = false;
+	yPos = $state(0);
 
 	setDarkMode(dark: boolean) {
 		this.isDarkMode = dark;
 	}
+
+	updateScroll(e: UIEvent & { currentTarget: EventTarget & HTMLElement; }) {
+		const deltaY = e.currentTarget.scrollTop - this.yPos;
+		const mod = this.isTouching ? 1.1 : 2;
+		this.yPos = e.currentTarget.scrollTop;
+		this.yClamped = clamp(this.yClamped - deltaY / mod, -55, 0);
+	}
+}
+
+function clamp(val: number, min: number, max: number) {
+	return Math.max(Math.min(val, max), min);
 }
 
 export const app = new AppState();
--- src/routes/(authenticated)/+page.svelte
+++ src/routes/(authenticated)/+page.svelte
@@ -2,16 +2,18 @@
 	import { afterNavigate, pushState, replaceState } from '$app/navigation';
 	import { page } from '$app/state';
 	import AddBookmarkModal from '$lib/client/AddBookmarkModal.svelte';
+	import { app } from '$lib/client/app.svelte';
 	import BookmarkList from '$lib/client/BookmarkList.svelte';
 	import EditBookmarkModal from '$lib/client/EditBookmarkModal.svelte';
 	import Header from '$lib/client/Header.svelte';
 	import { searchStore } from '$lib/client/search.svelte';
 	import Sidebar from '$lib/client/Sidebar.svelte';
-	import { IconMinus } from '@tabler/icons-svelte';
-	import type { PageServerData } from './$types';
-	import { tick } from 'svelte';
-	import { addQueryParam, removeQueryParam } from '$lib/client/util';
 	import { toastStore } from '$lib/client/toast.svelte';
+	import { toggleQueryParam } from '$lib/client/util';
+	import { IconMinus } from '@tabler/icons-svelte';
+	import { tick } from 'svelte';
+	import { flip } from 'svelte/animate';
+	import type { PageServerData } from './$types';
 
 	let { data }: { data: PageServerData } = $props();
 	let isScrolled = $state(false);
@@ -53,6 +55,8 @@
 	onscrollcapture={() => {
 		isScrolled = (mainEl?.scrollTop ?? 0) > 0;
 	}}
+	ontouchstart={() => (app.isTouching = true)}
+	ontouchend={() => (app.isTouching = false)}
 />
 
 <div class="flex h-screen flex-col">
@@ -62,39 +66,37 @@
 		<Sidebar {handleAddBookmark} categories={data.categories} bind:isMenuOpen />
 
 		<main
-			class="max-h-[calc(100dvh-68px)] flex-1 overflow-auto p-4 pb-30 md:p-6 lg:p-8"
+			class="relative max-h-[calc(100dvh-68px)] flex-1 overflow-auto p-2"
+			onscroll={(e) => app.updateScroll(e)}
 			bind:this={mainEl}
 		>
-			<div class="ignore-scroll mx-auto max-w-6xl">
-				{#if data.filteredTag}
-					<a
-						href={removeQueryParam(page.url, 'tag')}
-						class="mb-4 flex w-max min-w-max items-center gap-1 rounded-full bg-blue-100 px-2 py-0.5 text-xs font-medium whitespace-nowrap text-blue-800 dark:bg-blue-900 dark:text-blue-100"
-					>
-						{data.filteredTag.name}
-
-						<IconMinus class="w-3"></IconMinus>
-					</a>
-				{:else}
-					<div
-						class="-mx-4 mb-3 flex items-center gap-2 overflow-y-auto px-4 pb-1 md:mx-0 md:max-w-full md:px-0"
-					>
+			{#if data.tags.length}
+				<div
+					class="bg-accent-700/10 dark:bg-accent-100/10 sticky top-0 z-10 mx-auto w-max max-w-[min(72rem,100%)] rounded-full py-2 backdrop-blur-xs"
+					style:translate="0px {app.yClamped}px"
+				>
+					<div class="scrollbar-none mask flex items-center gap-2 overflow-y-auto rounded-md px-2">
 						{#each data.tags as tag (tag.id)}
-							{#if tag.name}
-								<a
-									href={addQueryParam(page.url, 'tag', tag.id)}
-									class="bg-accent-100 text-accent-800 dark:bg-accent-900 dark:text-accent-100 flex w-max w-max max-w-[calc(100%+2rem)] min-w-max gap-1 rounded-full px-2 py-1.5 pr-3 text-xs font-medium whitespace-nowrap"
-								>
-									{tag.name}
+							{@const show = tag.name && (!data.filteredTag || data.filteredTag.id === tag.id)}
+							<a
+								animate:flip={{ duration: 150 }}
+								href={toggleQueryParam(page.url, 'tag', tag.id)}
+								class:hidden={!show}
+								class="bg-accent-100 text-accent-800 dark:bg-accent-900 dark:text-accent-100 flex w-max w-max max-w-[calc(100%+2rem)] min-w-max items-center gap-1 rounded-full px-2 py-1.5 text-xs font-medium whitespace-nowrap"
+							>
+								{tag.name}
 
-									{#if tag.count > 1}
-										<span class="font-normal text-slate-400">{tag.count}</span>
-									{/if}
-								</a>
-							{/if}
+								{#if data.filteredTag?.id === tag.id}
+									<IconMinus class="h-3"></IconMinus>
+								{:else if tag.count > 1}
+									<span class="pr-1 font-normal text-slate-400">{tag.count}</span>
+								{/if}
+							</a>
 						{/each}
 					</div>
-				{/if}
+				</div>
+			{/if}
+			<div class="ignore-scroll mx-auto max-w-6xl p-2 !pt-2 pb-24 md:p-4 lg:p-6">
 				<BookmarkList bookmarks={data.bookmarks} addBookmark={handleAddBookmark} />
 			</div>
 		</main>
@@ -122,7 +124,7 @@
 		<div class="fixed right-4 bottom-4 z-50 flex flex-col gap-2">
 			{#each toastStore.toasts as toast (toast.id)}
 				<div
-					class="animate-fade-in rounded-md bg-gray-800 px-4 py-2 text-white shadow-lg border-l-4 border-primary-500"
+					class="animate-fade-in border-primary-500 rounded-md border-l-4 bg-gray-800 px-4 py-2 text-white shadow-lg"
 					role="alert"
 				>
 					<p>{toast.message}</p>
@@ -131,3 +133,13 @@
 		</div>
 	{/if}
 </div>
+
+<style>
+	.scrollbar-none {
+		scrollbar-width: none;
+	}
+
+	.mask {
+		mask: linear-gradient(to right, transparent, black 12px, black calc(100% - 12px), transparent);
+	}
+</style>
