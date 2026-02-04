54b48b071b09e5e12f7ee5298fa67dd722abc32f
Philip Oesterlin <philip.oesterlin@gmail.com>
2026-02-04 22:12:43 +0100

    Updates challenge token unique constraint
    Changes the unique constraint on the challenge token
    table from the `token` column to the `id` column.
    
    This ensures that each challenge token has a unique ID.

--- drizzle/0004_outstanding_giant_man.sql
+++ drizzle/0004_outstanding_giant_man.sql
@@ -2,7 +2,7 @@ CREATE TABLE "challenge_token" (
 	"id" text NOT NULL,
 	"user_id" text,
 	"expires_at" timestamp with time zone NOT NULL,
-	CONSTRAINT "challenge_token_unique" UNIQUE("token")
+	CONSTRAINT "challenge_token_unique" UNIQUE("id")
 );
 --> statement-breakpoint
 ALTER TABLE "challenge_token" ADD CONSTRAINT "challenge_token_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE cascade;
